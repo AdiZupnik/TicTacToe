8115d9245aa03ba860fabc3d2b2a1ffb9e22508b
AdiZupnik <zupnikadi@gmail.com>
2023-12-22 10:34:54 +0200

    new button name

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     String turn;
@@ -17,13 +19,60 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     private void onNewGame() {
         board = new String[3][3];
-        for (int row=0; row < 3; row++)
-            for (int col=0; col < 3; col++)
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
                 board[row][col] = new String();
 
         turn = "X";
         count = 0;
+    }
+
+    public void onButtonClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_00:
+                handleClick(0, 0, R.id.btn_00);
+                break;
+            case R.id.btn_01:
+                handleClick(0, 1, R.id.btn_01);
+                break;
+
+        }
+    }
+
+    private void handleClick(int row, int col, int id) {
+        if (board[row][col].equals("")) {
+            board[row][col] = turn;
+            Button btn = findViewById(id);
+            btn.setText(turn);
+            onTurnEnd();
+        }
+    }
+
+    private void onTurnEnd() {
+        // בדיקה האם יש מנצח - חשוב לבצע לפני הבדיקה אם הלוח מלא
+        if (isWinner())
+            endGame(turn + " won!");
+        else {
+            count++;
+            // בדיקת מצב לוח מלא (תיקו)
+            if (count == 9)
+                endGame("Tie");
+            else {
+                // העברת התור
+                turn = (turn.equals("X") ? "O" : "X");
+            }
+        }
+
+    }
+
+    private void endGame(String s) {
+
+    }
+
+    private boolean isWinner() {
+        return false;
     }
 }
