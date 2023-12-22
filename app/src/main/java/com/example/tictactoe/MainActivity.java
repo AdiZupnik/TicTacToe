abc908de288c5dbff952f63e2196c8c22488a931
AdiZupnik <zupnikadi@gmail.com>
2023-12-22 11:20:31 +0200

    on button click

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onNewGame();
 
 
     }
@@ -31,16 +32,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_00:
+        if (view.getId() == R.id.btn_00)
                 handleClick(0, 0, R.id.btn_00);
-                break;
-            case R.id.btn_01:
+            if (view.getId() ==R.id.btn_01)
                 handleClick(0, 1, R.id.btn_01);
-                break;
-
+            if (view.getId() ==R.id.btn_02)
+                handleClick(0, 2, R.id.btn_02);
+            if (view.getId() ==R.id.btn_10)
+                handleClick(1, 0, R.id.btn_10);
+            if (view.getId() ==R.id.btn_11)
+                handleClick(1, 1, R.id.btn_11);
+            if (view.getId() ==R.id.btn_21)
+                handleClick(1, 2, R.id.btn_21);
+            if (view.getId() ==R.id.btn_20)
+                handleClick(2, 0, R.id.btn_20);
+            if (view.getId() ==R.id.btn_21)
+                handleClick(2, 1, R.id.btn_21);
+            if (view.getId() ==R.id.btn_22)
+                handleClick(2, 2, R.id.btn_22);
         }
-    }
 
     private void handleClick(int row, int col, int id) {
         if (board[row][col].equals("")) {
@@ -73,6 +83,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean isWinner() {
+        //is there a row with three the same
+        //is there a column with three the same
+        // is there a diagonal line with three the same
         return false;
     }
 }
